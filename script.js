641b1d06e2f236f4f53641d00468a52801d9ae39
Aslan Malsagov <tuesday@yandex-team.ru>
2021-12-13 00:45:44 +0300

    Добавил всплывашку для поиска на мобилках

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 $(document).ready(() => {
+	// Бургер
 	$('.category-buttons__burger').click(() => {
 		$('.drawer').removeClass('hidden-left');
 
 		setTimeout(() => {
 			$('.drawer__void').removeClass('hidden');
 		}, 200);
-	})
+	});
 
 	$('.drawer__void').click(() => {
 		$('.drawer').addClass('hidden-left');
 		$('.drawer__void').addClass('hidden');
+	})
+
+	$('.search-icon').click(() => {
+		if ($('.search input').css('display') == 'none') {
+			$('.search-popup').removeClass('hidden-top');
+		}
+	})
+
+	$('.search-popup__button-close').click(() => {
+		$('.search-popup').addClass('hidden-top');
 	})
 });
